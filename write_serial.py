217b3bc01c03448af3f7cf71455df774db0e2dae
bmcandy <rodneyfunnie@hotmail.com>
2021-07-29 18:03:40 +0100

    only simulates drivers in the entry list

--- write_serial.py
+++ write_serial.py
@@ -3,6 +3,7 @@
 import time
 import serial
 import random
+import MySQLdb #apt-get install python-mysqldb
 
 ser = serial.Serial (
 	port='/dev/ttyS0',
@@ -15,10 +16,26 @@ ser = serial.Serial (
 
 f = open("serialoutput.txt", "w")
 
+try:
+	conn = MySQLdb.connect(
+		user="Results",
+		passwd="Results",
+		host="localhost",
+		port=3306,
+		db="SpeedOnScreen"
+	)
+except MySQLdb.Error as e:
+	print(e)
+
 while 1:
-	car = str(random.randint(1,20))
-	if random.randint(1,20) == 1:
-		car = car + "A"
+	cur = conn.cursor()
+	cur.execute('''SELECT Car FROM Entries;''')
+	entries=cur.fetchall()
+	conn.commit()
+	randomentry = random.randint(0,len(entries)-1)
+	car = str(''.join(entries[randomentry]))
+#	if random.randint(1,20) == 1:
+#		car = car + "A"
 	finish = str(random.randint(34,63)) + "." + str(random.randint(10,99))
 	if random.randint(1,40) == 1:
 		finish = "NTR"
@@ -32,8 +49,8 @@ while 1:
 		f.write("Car: %s  Time: %s\r\n  %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime))
 	else:
 		print "no split time"
-#	interval = random.randint(2,5)
-	interval = random.randint(22,30)
+	interval = random.randint(2,5)
+#	interval = random.randint(22,30)
 	print "waiting %d seconds"%(interval)
 	time.sleep(interval)
 	if random.randint(1,20) == 1:
@@ -41,4 +58,4 @@ while 1:
 		f.write("End of Batch\n")
 		endbatch=random.randint(300,600)
 		print "End of batch.  Sleeping %d seconds"%(endbatch)
-		time.sleep(endbatch)
+		#time.sleep(endbatch)
