447865d05b7d22d8050c893dd7efac48791f5ac1
bristolmotorclub <43007222+bristolmotorclub@users.noreply.github.com>
2023-06-24 11:36:55 +0100

    Printing anomalies to screen
    chore: user feedback for anomalies

--- write_serial.py
+++ write_serial.py
@@ -37,18 +37,24 @@ while 1:
 	car = str(''.join(entries[randomentry]))
 	# Add an "A" car that probably doesn't exist every ~20 cars
 	if random.randint(1,20) == 1:
+		print "Invalid car"
 		car = car + "A"
 	finish = str(random.randint(34,63)) + "." + str(random.randint(10,99))
 	# Report car as no time recorded every ~40 cars
 	if random.randint(1,40) == 1:
+		print "NTR"
 		finish = "NTR"
 	# Report car as red flagged every ~100 cars
 	if random.randint(1,100) == 1:
+		print "Red Flag"
 		finish = "Red Flag"
 	# Generate a 64ft/split time 99% of the time
 	if random.randint(1,100) > 1:
 		sixfour = str(random.randint(2,4)) + "." + str(random.randint(10,99))
+	else:
+		print "No 64ft time"
 	if random.randint(1,100) > 1:
+		print "No split time"
 		splittime = str(random.randint(22,28)) + "." + str(random.randint(10,99))
 	print "Car: %s  Time: %s\r\n   %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime)
 	ser.write("Car: %s  Time: %s\r   %s  %s\rxxx"%(car,finish,sixfour,splittime))
