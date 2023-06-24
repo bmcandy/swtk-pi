18efcfe0e71c8906f2b9bb1406fcb39f758b3e1b
bristolmotorclub <43007222+bristolmotorclub@users.noreply.github.com>
2023-06-24 12:05:50 +0100

    chore: removed extra variable type printing to screen

--- read_serial.py
+++ read_serial.py
@@ -77,7 +77,7 @@ def UpdateResults(carno="",ftime="",rs=""):
 				else:
 					colcolour = "00c2cb"
 					break
-				print str(type(ftime))
+				#print str(type(ftime))
 				if float(classfastest[1]) > float(ftime) and (classfastest[1]) < 999.99:
 
 					print "\n\n*** Fastest ***\n\n"
