c4aa9fd1eba00311751aea5af426c4182ba5b1e8
bristolmotorclub <43007222+bristolmotorclub@users.noreply.github.com>
2023-06-24 11:34:09 +0100

    Virtual event update
    feat: added a random car every ~20
    chore: added some comments

--- write_serial.py
+++ write_serial.py
@@ -28,31 +28,37 @@ except MySQLdb.Error as e:
 	print(e)
 
 while 1:
+	# Select a random entry from the database
 	cur = conn.cursor()
 	cur.execute('''SELECT Car FROM Entries;''')
 	entries=cur.fetchall()
 	conn.commit()
 	randomentry = random.randint(0,len(entries)-1)
 	car = str(''.join(entries[randomentry]))
-#	if random.randint(1,20) == 1:
-#		car = car + "A"
+	# Add an "A" car that probably doesn't exist every ~20 cars
+	if random.randint(1,20) == 1:
+		car = car + "A"
 	finish = str(random.randint(34,63)) + "." + str(random.randint(10,99))
+	# Report car as no time recorded every ~40 cars
 	if random.randint(1,40) == 1:
 		finish = "NTR"
+	# Report car as red flagged every ~100 cars
 	if random.randint(1,100) == 1:
 		finish = "Red Flag"
+	# Generate a 64ft/split time 99% of the time
 	if random.randint(1,100) > 1:
 		sixfour = str(random.randint(2,4)) + "." + str(random.randint(10,99))
+	if random.randint(1,100) > 1:
 		splittime = str(random.randint(22,28)) + "." + str(random.randint(10,99))
-		print "Car: %s  Time: %s\r\n   %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime)
-		ser.write("Car: %s  Time: %s\r   %s  %s\rxxx"%(car,finish,sixfour,splittime))
-		f.write("Car: %s  Time: %s\r\n   %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime))
-	else:
-		print "no split time"
-	interval = random.randint(2,5)
-#	interval = random.randint(22,30)
+	print "Car: %s  Time: %s\r\n   %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime)
+	ser.write("Car: %s  Time: %s\r   %s  %s\rxxx"%(car,finish,sixfour,splittime))
+	f.write("Car: %s  Time: %s\r\n   %s  %s\r\n\nxxx\n"%(car,finish,sixfour,splittime))
+	# Wait until starting the next car
+	interval = random.randint(2,5)	#2-5 seconds (fast testing)
+#	interval = random.randint(22,30) # 22-30 seconds (realistic)
 	print "waiting %d seconds"%(interval)
 	time.sleep(interval)
+	# Add end of batch every ~20 cars
 	if random.randint(1,20) == 1:
 		ser.write("End of Batch\n")
 		f.write("End of Batch\n")
