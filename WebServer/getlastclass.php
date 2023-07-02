206c62fa995e98f0c2cfb16fe216860c0f48f819
bristolmotorclub <43007222+bristolmotorclub@users.noreply.github.com>
2023-07-02 17:12:28 +0100

    Removed bug where it looked for a class attribute before returning

--- WebServer/getlastclass.php
+++ WebServer/getlastclass.php
@@ -2,10 +2,6 @@
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-if (!isset($_GET['class'])) {
-    echo("[]");
-    exit;}
-$class = $_GET['class'];
 include "database.php";
 //SQL Query
 $sql = 'SELECT Entries.Driver, sub.Best, Entries.Class FROM ( SELECT Car,Least(COALESCE(Timed1,Timed2),COALESCE(Timed2,Timed1),COALESCE(Timed3,Timed1),COALESCE(Timed4,Timed1)) AS Best from ClassResults where Class = (select Class from Entries where Car=(SELECT Car FROM RawResults order by TimeOfDay Desc LIMIT 1)) order by Best ) sub LEFT JOIN Entries ON sub.Car=Entries.Car order by CASE WHEN Best IS NULL THEN 1 ELSE 0 END ASC, Best Asc';
