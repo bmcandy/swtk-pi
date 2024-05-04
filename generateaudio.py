99d0d8ce02dd7e1c1feaab87ac7dfa53a825ea7d
bristolmotorclub <rodneyfunnie@hotmail.com>
2024-05-04 16:08:29 +0100

    feat: added Flask API to audio
    bug: Flask API only works once.

--- generateaudio.py
+++ generateaudio.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import pyttsx3  # pip install pyttsx3
 
 engine = pyttsx3.init()
@@ -23,6 +23,11 @@ def speak_finish_time(car, driver, time, split, sixtyfour=None):
     print(message)
     engine.say(message)
     engine.runAndWait()
+    engine.startLoop(False)
+    # engine.iterate() must be called inside Server_Up.start()
+    # Server_Up = threading.Thread(target=Comm_Connection)
+    # Server_Up.start()
+    engine.endLoop()
 
 
 def speak_class_results(thisclass):
@@ -30,4 +35,19 @@ def speak_class_results(thisclass):
     print("Results for class " + thisclass)
 
 
-speak_finish_time("69", "Archie Laurence", "91.34", "46.63")
+app = Flask(__name__)
+
+
+@app.route("/finish")
+def car_finished():
+    car = request.args.get("car", default=0, type=str)
+    driver = request.args.get("driver", default=0, type=str)
+    finish = request.args.get("finish", default=0, type=str)
+    split = request.args.get("split", default=0, type=str)
+    sixtyfour = request.args.get("sixtyfour", default=0, type=str)
+    speak_finish_time(car, "Archie Laurence", "91.34", "46.63")
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
